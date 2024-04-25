2fe02061662096d09e0ac54eda7584b32ecdc07e
Pedro <pedrofernandesdelfino@gmail.com>
2024-04-25 14:06:28 -0300

    GET endpoint for Musicas

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,5 +1,15 @@
+using System.Text.Json.Serialization;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    var dal = new Dal<Artista>(new ScreenSoundContext());
+    return dal.Listar();
+});
 app.Run();
