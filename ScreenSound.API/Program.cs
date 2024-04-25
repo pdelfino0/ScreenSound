37b19defab7cfe955d0569ecd0402552af3f8c5b
Pedro <pedrofernandesdelfino@gmail.com>
2024-04-25 14:25:37 -0300

    GET endpoint with Artist name as parameter

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -7,9 +7,21 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/artistas", () =>
 {
     var dal = new Dal<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+app.MapGet("/artistas/{nome}", (string nome) =>
+{
+    var dal = new Dal<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome == nome);
+
+    if (artista == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(artista);
 });
 app.Run();
--- ScreenSound.Shared.Modelos/Modelos/Artista.cs
+++ ScreenSound.Shared.Modelos/Modelos/Artista.cs
@@ -4,6 +4,9 @@ public class Artista
 {
     public virtual ICollection<Musica> Musicas { get; set; } = new List<Musica>();
 
+    public Artista()
+    {
+    }
     public Artista(string nome, string bio)
     {
         Nome = nome;
